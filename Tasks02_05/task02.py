4a2a8b569eabdab9cb729badb591b26728bc08ae
Yury Balykov <juser89@mail.ru>
2021-06-24 13:10:11 +0300

    task02

--- Tasks02_05/task02.py
+++ Tasks02_05/task02.py
@@ -10,4 +10,13 @@ from typing import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    ...
+    """
+    checking sequence is it a Fibonacci sequence
+    if sequence contain only 1 number -> it's not sequence -> return: False
+    :param data: Sequence[int]
+    :return: True/False
+    """
+    fib_sequence = [0, 1]
+    for i in range(len(data) - 2):
+        fib_sequence.append(fib_sequence[i] + fib_sequence[i + 1])
+    return fib_sequence == data
---
+++ tests/test_task02.py
@@ -0,0 +1,19 @@
+import pytest
+from Tasks02_05.task02 import check_fibonacci
+
+
+def test_positive_case():
+    """Testing that sequence is a Fibonacci sequence"""
+    assert check_fibonacci([0, 1, 1, 2, 3, 5])
+
+
+def test_negative_case():
+    """Testing that sequence i'snt a Fibonacci sequence"""
+    assert not check_fibonacci([0, 1, 1, 11, 3, 5])
+
+
+def test_with_one_number():
+    """Testing that sequence i'snt a Fibonacci sequence because has only one number"""
+    assert not check_fibonacci([17])
+
+

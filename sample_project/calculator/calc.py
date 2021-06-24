bb985a4b6d9d4486970dd892aff9fe24c442db4d
Yury Balykov <juser89@mail.ru>
2021-06-24 12:28:22 +0300

    task_01

--- sample_project/calculator/calc.py
+++ sample_project/calculator/calc.py
@@ -5,4 +5,12 @@ Task01 from homework1: fix all bugs in the `sample_project` code
 
 
 def check_power_of_2(a: int) -> bool:
-    return not (bool or (a & (a - 1)))
+    """
+    Checking number is it power of 2
+    :param a: integer
+    :return: True/False
+    """
+
+    return not (bool(a == 0 or (a & (a - 1))))
+
+print(check_power_of_2(-1))

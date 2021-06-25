1bb020ad4aca2ae71c0ed271ddacdae5bc6fafa9
Yury Balykov <juser89@mail.ru>
2021-06-25 15:43:59 +0300

    task04

---
+++ tests/test_task04.py
@@ -0,0 +1,11 @@
+import pytest
+from Tasks02_05.task04 import check_sum_of_four
+
+
+def test_equal_values():
+    """Testing that function calculates combination in lists with numbers that give zero"""
+    l1 = [1, -2, 4, -5, 9]
+    l2 = [1, -4, -6, 11, 10]
+    l3 = [-4, 7, -2, 11, 1]
+    l4 = [-10, 8, -7, 6, 20]
+    assert check_sum_of_four(l1, l2, l3, l4) == 15

9ec0ccc03813035c0b36930cfdae31b720fe47bf
Yury Balykov <juser89@mail.ru>
2021-07-06 11:09:21 +0300

    fix test_task03, add init files

--- homework/tests/test_task03.py
+++ homework/tests/test_task03.py
@@ -5,10 +5,11 @@ from homework.Tasks02_05.task03 import find_maximum_and_minimum
 
 def test_find_maximum_and_minimum_in_file_with_nums():
     """Testing that function find min and max in file"""
-    path_absolute = os.path.abspath("some_file.txt")
-    assert find_maximum_and_minimum(path_absolute) == [
-        (1, 613),
-        (4, 190),
-        (-12, 16),
-        (0, 0),
+    text_file = open("text_with_nums.txt", "w")
+    text_file.write("1,2,3,5,613")
+    text_file.close()
+    path_dir = os.getcwd()
+    assert find_maximum_and_minimum(os.path.join(path_dir, "text_with_nums.txt")) == [
+        (1, 613)
     ]
+    os.remove(os.path.join(path_dir, "text_with_nums.txt"))

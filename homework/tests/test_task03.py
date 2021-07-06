2e66f0a54dcfb5809d6c5d992562b47b9befd99a
Yury Balykov <juser89@mail.ru>
2021-07-06 15:29:51 +0300

    fix slash sign

--- homework/tests/test_task03.py
+++ homework/tests/test_task03.py
@@ -7,5 +7,5 @@ def test_find_maximum_and_minimum_in_file_with_nums():
     """Testing that function find min and max in file"""
     path_dir = os.getcwd()
     assert find_maximum_and_minimum(
-        os.path.join(path_dir, "homework\\tests\\some_file.txt")
+        os.path.join(path_dir, "homework/tests/some_file.txt")
     ) == [(1, 613)]
